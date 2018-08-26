458e6faaef5be539da83956790d69b78e95eaa5a
Shreyas H N <shreyas@MacBook-Pro-2.local>
2018-08-26 22:34:37 +0530

    added file open functionality

---
+++ utility.h
@@ -0,0 +1,9 @@
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <iomanip>
+#include <sys/param.h>
+#include <stdio.h>
+#include <sys/dir.h>
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
